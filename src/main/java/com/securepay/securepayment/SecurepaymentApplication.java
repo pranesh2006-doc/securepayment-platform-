7f71ac3fdf9241234b7988b5b2f60810808493b2
pranesh2006-doc <praneshdevaraj0@gmail.com>
2026-08-21 09:51:32 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/securepay/securepayment/SecurepaymentApplication.java
@@ -0,0 +1,13 @@
+package com.securepay.securepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurepaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/securepay/securepayment/SecurepaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securepay.securepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurepaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
